cca88a80824b883a279e78b22ba0bc6909fa9238
yjx <2472592648@qq.com>
2024-01-09 17:30:06 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import {} from "vue"
+import Person from "./components/Person.vue"
+</script>
+
+<template>
+    <Person />
+    <Person />
+</template>
+
+<style lang="scss" scoped></style>
